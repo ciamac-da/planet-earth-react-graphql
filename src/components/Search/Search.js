31a446b5b4c456444782c37bf01eea1289876a16
ciamac-da <ciamac.davoudi@gmail.com>
2021-02-27 10:58:06 +0100

    Verified by Marti4n v1.3.9

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,38 +2,28 @@ import React, { useState } from 'react';
 import { AlertTitle } from '@material-ui/lab';
 import SearchIcon from '@material-ui/icons/Search';
 import { Input } from '@material-ui/core';
-import { useLazyQuery } from '@apollo/client';
-import gql from 'graphql-tag';
+import { Typography, Button } from '@material-ui/core/';
+import { NavLink } from "react-router-dom";
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import myStyle from "./SearchStyle";
-import { Countries } from '../Countries/CountriesTable';
-import { useQuery } from '@apollo/react-hooks';
-import { GET_COUNTRIES } from '../../graphql/get-countries';
 
 
 export default function SearhFilter(){
 const classes = myStyle()
-const [mySearchFilter, mySetSearchFilter] = useState('')
-const { data: { countries = []} = {}  } = useQuery(GET_COUNTRIES,{
-});
-console.log(mySetSearchFilter)
     return(
         <>
         <AlertTitle className={classes.mainStyle}>
-          <SearchIcon /> 
-           &nbsp;
-           Search Countries based on Languages...
+         <SearchIcon /> &nbsp; Searching through Countries based on Languages...
           <Input
-             type="text"
-             placeholder="Search..."
-             autoComplete="off"
-             onChange={(e) => mySetSearchFilter(e.target.value)}
-             className={classes.inputStyle}
-           />
-             {countries &&
-        countries.map((country, index) => (
-          <p key={country.name} myCountries={country} index={index} />
-        ))}
-       </AlertTitle>
+           type="text"
+           placeholder="Search a Language..."
+           autoComplete="off"
+           className={classes.inputStyle}
+          />
+           <NavLink     className={classes.linkStyle} to="/"          >                                                                       
+            <Button     className={classes.myButton}                  >   <NavigateBeforeIcon />   Back to Main   </Button>   
+           </NavLink>    
+      </AlertTitle>
       </>
     )
 }
--- src/components/Search/SearchStyle.js
+++ src/components/Search/SearchStyle.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   
   mainStyle:{
     backgroundColor:"#f58d06",
@@ -14,12 +14,41 @@ const useStyles = makeStyles(() => ({
     fontWeight:"bolder !important",
     flexDirection:"column",
     fontSize:"25px",
+    position:"relative",
+    top:"15px",
+    [theme.breakpoints.down('sm')]: {
+      position:"relative",
+      top:"40px"
+    },
   },
       inputStyle:{
             margin: "0 auto",
             textAlign:"center !important",
-            textAlignLast: "center"
+            textAlignLast: "center",
+           
           },
+          myButton :{
+            margin: "0 auto",
+            textAlign:"center !important",
+            color: "#fff",
+            backgroundColor:"rgb(29, 148, 0)",
+            transform: "translateX(0px) !important",
+            transition: ".5s",
+            position:"relative",
+            top:"40px",
+            "&:hover":{
+                transform: "translateX(10px) !important",
+                transition: ".5s",
+                backgroundColor:"#0067b3"
+          },
+          [theme.breakpoints.down('sm')]: {
+            position:"relative",
+            top:"30px"
+          },
+          },
+          linkStyle:{
+            textDecoration:"none !important",
+      }
        
 }));
 
